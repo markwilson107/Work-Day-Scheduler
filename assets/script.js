d7b2cb9d5127610f8840c90a3b2f73985d0190b1
Mark Wilson <markwilson107@hotmail.com>
2020-09-25 08:36:49 +0800

    Colour coded rows. Done.

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,28 @@
 $(document).ready(function () {
+    //---Time (Current hour in 24h time)---
+    const currentHour = parseInt(moment().format('HH'));
+    
     //---Date---
-    // Create an array for each month name
-    const monthNames = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"];
-    // Create an array for each day of the week
-    const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    // Create function for day suffix
-    const nth = function (n) {
-        if (n > 3 && n < 21) return 'th';
-        switch (n % 10) {
-            case 1: return "st";
-            case 2: return "nd";
-            case 3: return "rd";
-            default: return "th";
-        }
-    }
-    // Get date data
-    const d = new Date();
-    // Adds date data too #currentDay
-    $("#currentDay").text(dayNames[d.getDay()] + ", " + monthNames[d.getMonth()] + " " + d.getDate() + nth(d.getDate()));
+    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'));
 
-    //---Retrieve local storage data---
+    //---Retrieve local storage data & colour code rows---
     for (var i = 0; i < 9; i++) {
+        // Test if local storage data exists
         if (localStorage.getItem("rowText" + i) !== null) {
+            // Sets row text to local storage data
             $("#rowText" + i).text(localStorage.getItem("rowText" + i));
+        }
+        // Test if row is current
+        if ($("#rowText" + i).attr("hour") === currentHour) {
+            $("#rowText" + i).addClass("present");
+        }
+        // Tests if row is future
+        else if ($("#rowText" + i).attr("hour") >= currentHour) {
+            $("#rowText" + i).addClass("future");
+        }
+        // Tests if row is past
+        else if ($("#rowText" + i).attr("hour") <= currentHour) {
+            $("#rowText" + i).addClass("past");
         }
     }
 })
@@ -33,7 +32,7 @@ $("button").on("click", function () {
     var btnIndex = $(this).attr("index");
     var userInput = $("#rowText" + btnIndex).val();
     if (userInput !== "") {
-        // Saves current hour text to local storage
+        // Saves corresponding text to local storage
         localStorage.setItem("rowText" + btnIndex, userInput);
     }
 })
